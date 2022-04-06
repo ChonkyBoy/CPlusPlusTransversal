e96b9a5f51b922e8c91d468693eca628172f4555
ChonkyBoy <you@example.com>
2022-04-06 11:28:19 +0200

    paquetCarte get shuffled haha L and W 4 me

---
+++ cmake-build-debug/CMakeFiles/CplusplusT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CplusplusT.dir/main.cpp.obj"
+  "CMakeFiles/CplusplusT.dir/main.cpp.obj.d"
+  "CplusplusT.exe"
+  "CplusplusT.exe.manifest"
+  "CplusplusT.pdb"
+  "libCplusplusT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CplusplusT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
