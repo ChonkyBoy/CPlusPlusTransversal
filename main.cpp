409ddcfa7b7b5146f11c517d0da89c5740011d96
ChonkyBoy <you@example.com>
2022-04-06 11:28:07 +0200

    paquetCarte get shuffled haha L and W 4 me

--- main.cpp
+++ main.cpp
@@ -7,6 +7,16 @@
 #include <algorithm>
 #include <random>
 
+//Faire le paquet : OK
+//Mélanger le paquet : OK
+//Pas besoin de remagner le paquet car dès qu'on ped le paquet est remélangé pour éviter la triche hehe
+//Rouge ou noir :
+//+ ou - :
+//purple :
+//le tout qui marche :
+
+
+
 using namespace std;
 
 
@@ -39,21 +49,26 @@ public:
             cout << "Voici les cartes :" << " as" << " de " << this->signe << endl;
         }
         else {
-            cout << "Voici les cartes :" << this->chiffre << " de " << this->signe << endl;
+            cout << "Voici les cartes : " << this->chiffre << " de " << this->signe << endl;
         }
     }
 };
+
+//
 
 class Purple {
 private:
     vector<Carte*> paquetCarte;
 public:
 
-
-    void ajouterCartes(Carte* carte) {
-
+    void ajouterCartes(Carte* carte){
         this -> paquetCarte.push_back(carte);
     }
+    /*
+    void retirerCarte(){
+        this->paquetCarte.pop_back();
+    }
+     */
     /*void creerCarteCoeur(){
         for(int i = 1; i <= 13; i++) {
             Carte* carteCoeur = new Carte(i, "coeur", "rouge");
@@ -93,36 +108,74 @@ public:
     }
 
     void afficherPaquet(){
-        creerCarte();
         for(int i = 0; i < paquetCarte.size(); i++) {
+            cout << i;
             paquetCarte[i]->afficher();
         }
     }
 
+    void randomShuffleMarcheSTP() {
+        for (int i = 0; i < 52; i++) {
+            swap(paquetCarte[i], paquetCarte[rand() % 52]);
+        }
+        this->afficherPaquet();
+    }
+
+};
+
+class Choix {
+private:
+
+public:
 };
 
 int main() {
 
+    srand((unsigned)time(0));
+/*
+    Carte *asco = new Carte(1, "coeur", "rouge");
+    Carte *deuxco = new Carte(2, "coeur", "rouge");
+    Carte *troisco = new Carte(3, "coeur", "rouge");
+    Carte *quatreco = new Carte(4, "coeur", "rouge");
+    Carte *cinqco = new Carte(5, "coeur", "rouge");
+    Carte *sixco = new Carte(6, "coeur", "rouge");
+    Carte *septco = new Carte(7, "coeur", "rouge");
+    Carte *huitco = new Carte(8, "coeur", "rouge");
+    Carte *neufco = new Carte(9, "coeur", "rouge");
+    Carte *dixco = new Carte(10, "coeur", "rouge");
+    Carte *onzeco = new Carte(11, "coeur", "rouge");
+    Carte *douze = new Carte(12, "coeur", "rouge");
+    Carte *treizeco = new Carte(13, "coeur", "rouge");
+     */
+/*
+    for (int i = 0; i < 10; ++i) {
+        int valeur = rand();
+        cout << "valeur full rand mais pas trop lolilol" << valeur << endl;
+    }
+*/
+/*
+    srand((unsigned int) time(0));
+
+    for (int i = 0; i < 10; ++i) {
+        int valeur = rand();
+        cout << "valeur full rand mais pas trop lolilol" << valeur << endl;
+    }
+*/
+
+Purple* purple = new Purple;
+purple->creerCarte();
+purple->randomShuffleMarcheSTP();
 
 
-        /*
-        Carte *asco = new Carte(1, "coeur", "rouge");
-        Carte *deuxco = new Carte(2, "coeur", "rouge");
-        Carte *troisco = new Carte(3, "coeur", "rouge");
-        Carte *quatreco = new Carte(4, "coeur", "rouge");
-        Carte *cinqco = new Carte(5, "coeur", "rouge");
-        Carte *sixco = new Carte(6, "coeur", "rouge");
-        Carte *septco = new Carte(7, "coeur", "rouge");
-        Carte *huitco = new Carte(8, "coeur", "rouge");
-        Carte *neufco = new Carte(9, "coeur", "rouge");
-        Carte *dixco = new Carte(10, "coeur", "rouge");
-        Carte *onzeco = new Carte(11, "coeur", "rouge");
-        Carte *douze = new Carte(12, "coeur", "rouge");
-        Carte *treizeco = new Carte(13, "coeur", "rouge");
-         */
-        Purple *purple = new Purple();
-        purple->afficherPaquet();
-    random_shuffle(purple->afficherPaquet());
+
+/*
+    Purple* main_maitre;
+    Purple* table;
+
+    Carte* carte = main_maitre->retirerCarte();
+    table->ajouterCartes(carte);
+     Compare la carte avec la prédiction d jouer...
+*/
 
     return 0;
 }
